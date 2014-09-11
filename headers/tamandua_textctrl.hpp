58f4ba63d067ee935031fbd63f26d72936daf5b9
Kacper Kołodziej <kacper@kolodziej.in>
2014-09-11 17:28:45 +0200

    wxTextCtrl replaced with wxRichTextCtrl

--- headers/tamandua_textctrl.hpp
+++ headers/tamandua_textctrl.hpp
@@ -1,24 +1,24 @@
 #ifndef TAMANDUA_TEXTCTRL_HPP
 #define TAMANDUA_TEXTCTRL_HPP
 #include "wx/wx.h"
-#include "wx/textctrl.h"
+#include "wx/richtext/richtextctrl.h"
 #include <mutex>
 
 class tamandua_textctrl :
-	public wxTextCtrl
+	public wxRichTextCtrl
 {
 	private:
 		std::mutex write_lock_;
 	public:
 		tamandua_textctrl(wxWindow *win, wxWindowID id) :
-			wxTextCtrl(win, id, wxEmptyString, wxPoint(0,0), wxDefaultSize, wxTE_MULTILINE | wxTE_WORDWRAP | wxTE_READONLY | wxTE_AUTO_URL)
+			wxRichTextCtrl(win, id, wxEmptyString, wxPoint(0,0), wxDefaultSize, wxRE_MULTILINE | wxRE_READONLY)
 		{}
 
 		void add_message(wxString, wxString);
 		void add_info(wxString);
 		void add_error(wxString);
 		void add_warning(wxString);
-		void add_private_message(wxString, wxString);
+		void add_private_message(bool, wxString, wxString);
 };
 
 #endif
--- src/tamandua_textctrl.cpp
+++ src/tamandua_textctrl.cpp
@@ -1,84 +1,68 @@
 #include "tamandua_textctrl.hpp"
 
+#define Colour(r,g,b) BeginTextColour(wxColour(r,g,b))
+#define EndColour() EndTextColour();
+#define Nl() Newline();
+
 void tamandua_textctrl::add_message(wxString author, wxString msg)
 {
-	wxTextAttr author_attr(wxColour(0, 0, 0xff));
-	author_attr.SetFontWeight(wxFONTWEIGHT_BOLD);
-	wxString input(author + wxT(": ") + msg);
 	write_lock_.lock();
-	long int start = GetInsertionPoint(), end;
-	AppendText(author);
-	end = GetInsertionPoint();
-	AppendText(": ");
-	AppendText(msg);
-	AppendText("\n");
+	Colour(0x0b, 0x6c, 0xa1);
+	BeginBold();
+	WriteText(author);
+	EndBold();
+	EndColour();
+	WriteText(": ");
+	WriteText(msg);
+	Nl();
 	write_lock_.unlock();
-
-	SetStyle(start, end, author_attr);
 }
 
 void tamandua_textctrl::add_info(wxString info)
 {
-	wxTextAttr attr(wxColour(0x00, 0x99, 0));
-	attr.SetFontWeight(wxFONTWEIGHT_BOLD);
-	info += wxString("\n", wxMBConvUTF8());
-
 	write_lock_.lock();
-	long int start = GetInsertionPoint(), end;
-	AppendText(info);
-	end = GetInsertionPoint();
+	Colour(0x00, 0x99, 0);
+	BeginBold();
+	WriteText(info);
+	EndBold();
+	EndColour();
+	Nl();
 	write_lock_.unlock();
-
-	SetStyle(start, end, attr);
 }
 
 void tamandua_textctrl::add_error(wxString error)
 {
-	wxTextAttr attr(wxColour(0xcc, 0, 0));
-	attr.SetFontWeight(wxFONTWEIGHT_BOLD);
-	error += wxString("\n", wxMBConvUTF8());
-
 	write_lock_.lock();
-	long int start = GetInsertionPoint(), end;
-	AppendText(error);
-	end = GetInsertionPoint();
+	Colour(0xcc, 0, 0);
+	BeginItalic();
+	WriteText(error);
+	EndItalic();
+	EndColour();
+	Nl();
 	write_lock_.unlock();
-
-	SetStyle(start, end, attr);
 }
 
 void tamandua_textctrl::add_warning(wxString warning)
 {
-	wxTextAttr attr(wxColour(0xff, 0x53, 0x0d));
-	attr.SetFontWeight(wxFONTWEIGHT_BOLD);
-	wxString msg = wxT(" ! ") + warning + wxT("\n");
-
 	write_lock_.lock();
-	long int start = GetInsertionPoint(), end;
-	AppendText(msg);
-	end = GetInsertionPoint();
+	Colour(0xed, 0x7b, 0x00);
+	WriteText(warning);
+	EndColour();
+	Nl();
 	write_lock_.unlock();
-
-	SetStyle(start, end, attr);
 }
 
-void tamandua_textctrl::add_private_message(wxString author, wxString pm)
+void tamandua_textctrl::add_private_message(bool mymsg, wxString author, wxString pm)
 {
-	wxTextAttr author_attr(wxColour(0x40, 0x01, 0x27));
-	author_attr.SetFontWeight(wxFONTWEIGHT_BOLD);
-	wxTextAttr pm_attr(wxColour(0x14, 0x06, 0x40));
-	pm += wxT("\n");
-
-	wxString author_text = wxT("@") + author + wxT(": ");
-	
 	write_lock_.lock();
-	long int start = GetInsertionPoint(), mid, end;
-	AppendText(author_text);
-	mid = GetInsertionPoint();
-	AppendText(pm);
-	end = GetInsertionPoint();
+	Colour(0x4e, 0x13, 0x08);
+	if (mymsg)
+		WriteText(wxT("<-"));
+	else
+		WriteText(wxT("->"));
+	WriteText(author + wxT(": "));
+	EndColour();
+	WriteText(pm);
+	Nl();
 	write_lock_.unlock();
-
-	SetStyle(start, mid, author_attr);
-	SetStyle(mid, end, pm_attr);
 }
